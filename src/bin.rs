88f383e130caa5282f63a7f1c7ac42967f6ff480
Aydan Pirani <aydanpirani@gmail.com>
2021-11-08 00:45:06 -0600

    Implemented basic CLI

--- src/bin.rs
+++ src/bin.rs
@@ -1,33 +1,34 @@
-use sentiment_analyzer::analysis;
+use std::env;
+
+// TO USE, RUN:
+// $ rustc src/bin.rs
+// $ ./bin --flag
 
 fn main() {
-    // let messages = vec!()
-    let happy_words = "absolutely accepted acclaimed accomplish accomplishment achievement action active admire adorable adventure affirmative affluent agree agreeable amazing angelic appealing approve aptitude attractive awesome";
-    let sad_words = "blue dejected depressed despondent down droopy hangdog inconsolable low melancholic melancholy mirthless sad unhappy woebegone woeful";
-    
-    // let temp = analysis::analyze_sentiment("good good!".to_string());
-    // display(temp);
-    
-    let temp2 = analysis::analyze_sentiment("good good good good!".to_string());
-    display(temp2);
-    
-    // let good_temp = analysis::analyze_sentiment(happy_words.to_string());
-    // let bad_temp = analysis::analyze_sentiment(sad_words.to_string());
+    let arguments: Vec<String> = env::args().collect();
 
-    // display(good_temp);
-    // display(bad_temp);
-}
+    if arguments.len() < 2 {
+        // Temporary error handling - not enough flags passed in
+        // add in help message?
+        println!("Invalid input! (Case A)")
+    }   
+    
+    let input_format = arguments[1].to_lowercase(); //Second flag in the series (input format)
+    let split: Vec<&str> = input_format.split("=").collect(); //Second flag, but split in key-value pair
 
-fn display(a: sentiment::Analysis) {
-    println!("Overall score: {}", a.score);
-    println!("Comparative: {}", a.comparative);
-    println!("NEGATIVE:");
-    let n = a.negative;
-    println!("{}", n.score);
-    println!("{:?}", n.words);
-    println!("POSITIVE:");
-    let p = a.positive;
-    println!("{}", p.score);
-    println!("{:?}", p.words);
-    println!("--------------")
+    if split.len() != 2 {
+        // More temporary error handling - invalid flag passed in
+        println!("Invalid input! (Case B)");
+    }
+
+    match split[0] {
+        "--t" | "--twitter" => println!("Twitter"),
+        "--f" | "--file" => println!("File"),
+        _ => {
+            // Last potential case of error handling - flag passed in DNE
+            println!("Invalid input! (Case C)")
+        }
+    }
+
+
 }

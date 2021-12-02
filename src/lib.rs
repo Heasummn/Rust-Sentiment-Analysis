644f07ad484e9f9b2c2fe72f43a210b86b40bc02
Liza George <lizag2@illinois.edu>
2021-12-02 01:03:35 -0600

    Integrated map_reduce calls into main method, used num_cores to get best num_chunks, and tested everything!

--- src/lib.rs
+++ src/lib.rs
@@ -47,16 +47,39 @@ pub mod map_reduce{
     use std::sync::Mutex;
     use std::sync::Arc;
 
+    extern crate num_cpus;
+
+    /*
+    Separating away the logic for deciding how many chunks to split the input into.
+    It must be less than the input length for mapreduce to not break. 
+    Beyond that requirement, apparently for CPU bound loads like we have here the
+    ideal number of threads to produce is the number of cores the machine being used has. 
+    To find that out, I'm using the num_cpus crate (the get method incls 'hyperthreaded' cores 
+    and other tricks like that).
+    */
+    pub fn pick_num_chunks(input_len: usize) -> usize{
+        let num_cores = num_cpus::get();
+
+        if input_len < num_cores{
+            return input_len;
+        } else {
+            return num_cores-1; // the minus one is there bc the result of this function is the number of
+                                // additional threads we're creating, and ideally the number of threads this program
+                                // creates doesn't surpass the number of usable cores.
+        }
+    }
 
     /*
         Analyzes a vector of Messages to return a vector of AnalysisResults, but
         uses multi-threading to speed up the process. Each call of analysis is expensive,
-        but this way multiple calls of it can be called simultaneously. 
-        Also requires a 'num_chunks' usize in input, which is about equal to the number
-        of threads created. Must be between 0 and length of input. 
+        but this way multiple calls of it can be called simultaneously
     */
-    pub fn map_reduce( input: Vec<Message>, num_chunks: usize) 
+    pub fn map_reduce_messages_to_analyses( input: Vec<Message>) 
             -> Vec<AnalysisResult> {
+        // STEP 0: deciding number of chunks the input is divided into, which is equal
+        // to the number of threads created.
+        let num_chunks = pick_num_chunks(input.len());
+
 
         // <-----------STEP 1: MAP (split up input and create a thread for each sub-section that calls analysis)----------->
 
@@ -98,13 +121,13 @@ pub mod map_reduce{
         let remainder_handle = thread::spawn( move || -> Vec<AnalysisResult>{  
             let mut chunk_analysis:Vec<AnalysisResult> = Vec::new();
 
-            for j in 0..starting_size{
+            for j in 0..remainder{
                 let input_m = input_arc.lock().unwrap();
                 let m = &input_m[num_chunks*starting_size + j];
 
                 // need to manually deep clone Message to pass into analyze_sentiment...
                 let m_deep_clone = Message::new(m.text.to_owned(), m.time);
-                let a = analysis::analyze_sentiment(m_deep_clone); //TODO: .to_owned()?
+                let a = analysis::analyze_sentiment(m_deep_clone); 
                 chunk_analysis.push(a);
             }
 
